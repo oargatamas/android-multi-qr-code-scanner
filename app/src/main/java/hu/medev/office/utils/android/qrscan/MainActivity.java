d85d35f40a5e1dfb2487c7de6ab21150f452f552
toarga <oarga.tamas@gmail.com>
2021-07-01 21:55:58 +0200

    MainActivity changed to act as a home page.

--- app/src/main/java/hu/medev/office/utils/android/qrscan/MainActivity.java
+++ app/src/main/java/hu/medev/office/utils/android/qrscan/MainActivity.java
@@ -1,5 +1,6 @@
 package hu.medev.office.utils.android.qrscan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,19 +12,19 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import hu.medev.office.utils.android.R;
 import hu.medev.office.utils.android.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
+    private MainActivity activity;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.activity = this;
 
         hu.medev.office.utils.android.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -37,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent startScanIntent = new Intent(activity,ScannerActivity.class);
+                activity.startActivity(startScanIntent);
             }
         });
     }
